246d4890621e33dcf21fb93e2db6766153862f6f
naughtymor <znaknikita.it@gmail.com>
2022-12-11 21:15:00 +0300

    main block layout, styles

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,8 +2,20 @@ import style from "./Main.module.css"
 
 const Main = () => {
     return (
-        <div className={style.main}>
-
+        <div className={style.mainBlock}>
+            <div className={style.container}>
+                <div className={style.photo}></div>
+                <div className={style.text}>
+                    <h1>
+                        <span>NILSA</span>
+                        <span> </span>
+                        <span>BROWN</span>
+                    </h1>
+                    <h6>
+                        Creative Web & App Developer
+                    </h6>
+                </div>
+            </div>
         </div>
     )
 }
