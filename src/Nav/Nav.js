01682cefef6a670d67ca005bc6bc295902405913
naughtymor <znaknikita.it@gmail.com>
2023-01-04 20:49:45 +0300

    Navbar animation, refactoring

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,11 +1,22 @@
 import style from "./Nav.module.scss"
+import Slide from 'react-reveal/Slide';
+import { GrClose } from "react-icons/gr";
+
 
 const Nav = (props) => {
     return (
         <div className={style.navBlock} onClick={() => props.setActive(false)}>
-            <div className={style.nav} onClick={e => e.stopPropagation()}>
-
-            </div>
+            <Slide right>
+                <div className={style.nav} onClick={e => e.stopPropagation()}>
+                    <div className={style.closeButton} onClick={() => props.setActive(false)}><GrClose/></div>
+                    <h5>HOME</h5>
+                    <h5>SERVICES</h5>
+                    <h5>ABOUT ME</h5>
+                    <h5>FEEDBACK</h5>
+                    <h5>PORTFOLIO</h5>
+                    <h5>SKILLS</h5>
+                </div>
+            </Slide>
         </div>
     )
 }
