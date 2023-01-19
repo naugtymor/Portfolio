631abba5d942ee5425854e51e9296e652ec03d32
naughtymor <znaknikita.it@gmail.com>
2023-01-19 15:21:11 +0300

    mask to snowfalls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.module.scss';
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Skills from "./Skills/Skills";
@@ -11,14 +11,17 @@ import Modal from "./Modal/Modal";
 import {useState} from "react";
 import Snowfall from 'react-snowfall'
 import Nav from "./Nav/Nav";
+import style from "./App.module.scss"
+
 
 const App = () => {
     const [modalActive, setModalActive] = useState(false);
     const [navActive, setNavActive] = useState(false);
 
     return (
-        <div className="App">
-            <Snowfall color="#868a9b" style={{ height: '100%' }} snowflakeCount={100}/>
+        <div className={style.app}>
+            <Snowfall color="#868a9b" style={{ height: '100%' }} snowflakeCount={100}
+                      className={style.snowFalls}/>
             <Header setActive={setNavActive} navActive={navActive}/>
             <Main setActive={setModalActive}/>
             <Skills/>
