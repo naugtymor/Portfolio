37784d0b6738fd97ff029b5ca772fd041a7aebfe
naughtymor <znaknikita.it@gmail.com>
2023-01-24 21:39:23 +0300

    description to services

--- src/Skills/Skill/Skill.js
+++ src/Skills/Skill/Skill.js
@@ -7,11 +7,7 @@ const Skill = (props) => {
                 <span>
                     <div className={style.icon}>{props.icon}</div>
                     <h5>{props.title}</h5>
-                    <p className={style.description}>Website ravida surna eveti semen the conse tusio anivite dianne one
-                        nivam
-                        acestion vue artin
-                        dictum.
-                    </p>
+                    <p className={style.description}>{props.description}</p>
                     <div className={style.numb}>{props.num}</div>
                 </span>
             </div>
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -7,16 +7,19 @@ import { BsClockHistory, BsCodeSquare, BsLayoutWtf} from "react-icons/bs";
 const skills = [
     {
         title: 'WEB Development',
+        description: 'Web development services involve everything related to building a web-based solution – whether it is a simple text page or a complex web application. Many people think of “web development” as just using a variety of programming languages to create the web app code. ',
         num: '01',
         icon: <BsCodeSquare/>
     },
     {
         title: 'Application Design',
+        description: 'No matter how complex your product is, I know how to make its UX and UI design a web interface and experience that your users will love and want to buy. You can come to us with a detailed vision and requirements or let our designers fulfill your ideas from scratch.',
         num: '02',
         icon: <BsLayoutWtf/>
     },
     {
         title: 'SPA maintenance',
+        description: 'SPA is a web application or website that interacts with the web browser by dynamically rewriting the current web page with new data from the web server, instead of the default method of the browser loading entire new pages. SPA is a perfect solution.',
         num: '03',
         icon: <BsClockHistory/>
     },
@@ -26,12 +29,10 @@ const Skills = () => {
     return (
         <div className={style.skillsBlock} id={'services'}>
             <div className={`${style.skillsContainer} ${styleContainer.container}`}>
-                <Title mainTitle={"Services"} secondTitle={"SERVICES THAT I PROVIDE"} description={'Phasellus et lacus suscipit congue nisl the volutpat\n' +
-                    '                    magna. donec miss the drana risus tincidunt convallis velit orci congue tortor eu dignissim\n' +
-                    '                    ipsum suam non odio. Pellenta esuntion miss the imperdiet metus ornare. '}/>
+                <Title mainTitle={"Services"} secondTitle={"SERVICES THAT I PROVIDE"} description={'Here you can see the range of services and skills that I can provide you with accompanying your commercial project. '}/>
                 <div className={style.skills}>
                     {
-                        skills.map((s, index) => <Skill key={index} title={s.title} num={s.num} icon={s.icon}/>)
+                        skills.map((s, index) => <Skill key={index} title={s.title} num={s.num} icon={s.icon} description={s.description}/>)
                     }
                 </div>
             </div>
